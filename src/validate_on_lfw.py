c87b6fe06100e7f0f52b4ef842ee6246e8e68c6c
Bruceeeee <zhanghtzj@163.com>
2017-11-15 22:29:07 +0800

    add purning

--- src/validate_on_lfw.py
+++ src/validate_on_lfw.py
@@ -104,7 +104,7 @@ def parse_arguments(argv):
     parser.add_argument('--lfw_pairs', type=str,
         help='The file containing the pairs to use for validation.', default='data/pairs.txt')
     parser.add_argument('--lfw_file_ext', type=str,
-        help='The file extension for the LFW dataset.', default='png', choices=['jpg', 'png'])
+        help='The file extension for the LFW dataset.', default='jpg', choices=['jpg', 'png'])
     parser.add_argument('--lfw_nrof_folds', type=int,
         help='Number of folds to use for cross validation. Mainly used for testing.', default=10)
     return parser.parse_args(argv)
