0adfafe9d56bb61692e2323c00996889fc94130d
Bruceeeee <zhanghtzj@163.com>
2017-11-19 23:31:43 +0800

    fix some bug

--- src/facenet.py
+++ src/facenet.py
@@ -660,7 +660,7 @@ def get_masks(model_dir, percentile, output_file):
                       if 'Conv2d' in name else np.percentile(weight, percentile / 2.0)
                       for weight, name  in zip(numpy_weights, weight_name)]
         upper_thrs = [np.percentile(weight, 100 - 4 * percentile / 2.0) 
-                      if 'Conv2d' in name else np.percentile(weight, 1 - percentile / 2.0) 
+                      if 'Conv2d' in name else np.percentile(weight, 100 - percentile / 2.0) 
                       for weight in numpy_weights]
         masks = [(weight < lower_thr) + (weight > upper_thr)
                  for weight, lower_thr, upper_thr in zip(numpy_weights, lower_thrs, upper_thrs)]
