76accd0d752bd28f40bec8c069013a2071e4e463
Bruceeeee <zhanghtzj@163.com>
2017-12-11 21:40:01 +0800

    changed

--- src/pruning.py
+++ src/pruning.py
@@ -10,7 +10,6 @@ def get_masks(weights, percentile, layer_type=None):
     """ Use percentile to get weight mask"""
     weights_name = [tensor.name for tensor in tf.get_default_graph().get_operations()
                     if tensor.name.endswith('weights')]
-    print(weights_name)
     numpy_weights = np.array([weight.eval() for weight in weights])
     lower_thrs, upper_thrs = [], []
     for weight, name in zip(numpy_weights, weights_name):
@@ -19,6 +18,7 @@ def get_masks(weights, percentile, layer_type=None):
             upper_thr = np.percentile(
                 weight[weight != 0], 100 - percentile / 2.0)
         elif name in layer_type:
+            print(name)
             lower_thr = np.percentile(weight[weight != 0], percentile / 2.0)
             upper_thr = np.percentile(
                 weight[weight != 0], 100 - percentile / 2.0)
