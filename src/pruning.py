b5bc222bc0d28769c8dcee9ca98bdf3f2bced223
Bruceeeee <zhanghtzj@163.com>
2017-12-11 20:13:38 +0800

    changed

--- src/pruning.py
+++ src/pruning.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
         print(pruning_rate)
         for epoch, rate in pruning_rate:
             masks = get_masks(weights=weights, percentile=rate,
-                              layer_type='Repeat2')
+                              layer_type='Repeat_2')
             write_log(rate, 2, os.path.expanduser('~'))
             assign_all = apply_masks(weights, masks)
             sess.run(assign_all)
