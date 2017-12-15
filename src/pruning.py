cf8f8f01099a8534662cf3216c67de00de6c3dc0
Bruceeeee <zhanghtzj@163.com>
2017-12-15 15:31:53 +0800

    fix_layer

--- src/pruning.py
+++ src/pruning.py
@@ -13,9 +13,9 @@ def get_masks(weights, percentile, layer_type=None, fix_layer=None):
                     if tensor.name.endswith('weights')]
     numpy_weights = np.array([weight.eval() for weight in weights])
     masks = []
+    fix_op = (fix_layer is None)
     for weight, name in zip(numpy_weights, weights_name):
-        if fix_layer is not None:
-            if fix_layer in name:
+        if fix_op and fix_layer in name:
                 mask = (weight != 0)
         elif layer_type is None:
             lower_thr = np.percentile(weight[weight != 0], percentile / 2.0)
